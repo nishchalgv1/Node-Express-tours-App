820bc83d2ec49e5c815845d7754cd2b61c8b4099
Nishchal <nischal.15gupta@gmail.com>
2024-05-28 00:20:17 +0530

    Seperating Routes and RouteHandlers at one place

--- app.js
+++ app.js
@@ -8,79 +8,41 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 
-//before we can send the tours data, we need to first read it from the json file
+//read the data from the JSON file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-app.get('/api/v1/tours', (req, res) => {
-  //this callback function is called route handler
+
+//Refactoring our routes
+//Separating all the route handlers function together and all the routes together from each other
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
-    /*
-    when we are sending multiple responses is to include 
-    a field called results with the number of results that we are sending.
-    It makes very easy for the client to get very quick information about the data it is receiving.
-     */
     results: tours.length,
     data:{
       tours
     }
   })
-})
-//returning a tour by id url parameters, the variable that we define in the route are url parameters
-//they are made available for us in req.params
-app.get('/api/v1/tours/:id', (req, res) => {
-  //get tour by id
-  //first get the id
-  const id = req.params.id * 1;
-  /*Check if the id is larger than the length of the tours array and if it is longer 
-  then we can send back a  404 error saying that we could not find any tour for the given ID. */
-  if(id > tours.length){
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid Id"
-    })
-  }
-  const tour = tours.find(elem => elem.id === id);
- 
-  res.status(200).json({
-    status: "success",
-    data:{
-      tour
-    }
-  })
-})
+}
 
-//handling patch requests
-app.patch('/api/v1/tours/:id', (req, res) => {
-  if(req.params.id * 1 > tours.length){
+const getTour = (req, res) => {
+  const newId = (req.params.id) * 1;
+  if(newId > tours.length){
     return res.status(404).json({
       status: "fail",
       message: "Invalid ID"
     })
   }
-
+  const tour = tours.find(elem => elem.id === id);
+  
   res.status(200).json({
     status: "success",
-    message: "<Updated tour here>"
+    data: {
+      tour
+    }
   })
-})
+}
 
-//handling delete requests 
-app.delete('/api/v1/tours/:id', (req, res) => {
-  if(req.params.id * 1 > tours.length){
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid Id"
-    })
-  }
 
-  res.status(204).json({
-    //204 status code means no content
-    status: "success",
-    data: null
-  })
-})
-
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({id: newId}, req.body);
 
@@ -94,7 +56,44 @@ app.post('/api/v1/tours', (req, res) => {
       }
     })
   })
-})
+}
+
+const updateTour = (req, res) => {
+  if(req.params.id * 1 > tours.length){
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID"
+    })
+  }
+
+  res.status(200).json({
+    status: "success",
+    message: "<Updated tour here>"
+  })
+}
+
+const deleteTour = (req, res) => {
+  if(req.params.id * 1 > tours.length){
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Id"
+    })
+  }
+
+  res.status(204).json({
+    //204 status code means no content
+    status: "success",
+    data: null
+  })
+}
+
+app.get('/api/v1/tours', getAllTours);
+app.get('/api/v1/tours/:id', getTour);
+app.post('/api/v1/tours', createTour);
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
+
+
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 })
